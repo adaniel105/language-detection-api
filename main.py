a5eb65599b242e1f9a380509bf2ec53e2f554dda
Akinola Daniel <vantablack294@gmail.com>
2024-07-24 12:06:43 +0100

    setting up html template

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 from pydantic import BaseModel
 from models.model import prediction_pipeline
 
 
-
 app = FastAPI()
+
+template_folder = Path(__file__).resolve(strict=True).parent / "templates"
+templates = Jinja2Templates(directory=str(template_folder))
 
 
 class Prediction(BaseModel):
-  language : str
+    language: str
 
 
-@app.get("/")
-async def index():
-  return {"Health Check" : "OK"}
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request, prediction: str = ""):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "prediction": prediction}
+    )
 
 
-@app.post("/predict", response_model=Prediction)
-async def Predict(language: str):
-  preds = prediction_pipeline(language)
-  return {"language" : language}
-
-
-
-
+"""
+@app.post("/predict", response_class=HTMLResponse)
+async def predict(language: str):
+    preds = prediction_pipeline(language)
+    return {"language": preds}
+"""
 
 if __name__ == "__main__":
-  import uvicorn
-  uvicorn.run(app, host="127.0.0.1", port=8080)
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8080)
